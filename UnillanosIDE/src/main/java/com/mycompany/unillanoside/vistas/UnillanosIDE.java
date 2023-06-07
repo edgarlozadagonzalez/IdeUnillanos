62ec859dab10351176b841bbd1d0e4011626759f
David Lozada <edgar.lozada@unillanos.edu.co>
2023-06-06 20:33:04 -0500

    vista agregada para prueba

---
+++ UnillanosIDE/src/main/java/com/mycompany/unillanoside/vistas/UnillanosIDE.java
@@ -0,0 +1,9 @@
+package com.mycompany.unillanoside.vistas;
+
+public class UnillanosIDE {
+
+    public static void main(String[] args) {
+        Start ventana = new Start();
+        ventana.setVisible(true);
+    }
+}
